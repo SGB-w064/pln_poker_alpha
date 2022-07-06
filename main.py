86bc0977f510d16ebfec9688ce7117b405b84e5c
SGB-w064 <71425353+SGB-w064@users.noreply.github.com>
2022-07-06 10:03:55 +0900

    サブウィンドウを表示できるようにした

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!python3.8
 
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QDialog, QPushButton
 from PyQt6.QtCore import Qt
 
 class Window(QWidget):
@@ -17,14 +17,35 @@ class Window(QWidget):
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label)
 
-def createWindow():
+        sub_window_button = QPushButton("create\nsub window")
+        sub_window_button.clicked.connect(self.createWindow)
+        layout.addWidget(sub_window_button)
+
+    def createWindow(self):
+        new_window = SubWindow()
+        new_window.show()
+
+class SubWindow(QWidget):
+    def __init__(self):
+        self.w = QDialog()
+        self.w.resize(960, 540)
+        label = QLabel()
+        label.setText("sub")
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        self.w.setLayout(layout)
+
+    def show(self):
+        self.w.exec()
+
+def createMainWindow():
     app = QApplication([])
     window = Window()
     window.show()
     sys.exit(app.exec())
 
 def main():
-    createWindow()
+    createMainWindow()
 
 if __name__ == "__main__":
     main()
