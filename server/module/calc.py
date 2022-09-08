8243b804be27489025611e5bcde9d8629d258c9d
sgb-w064 <71425353+SGB-w064@users.noreply.github.com>
2022-09-09 04:11:29 +0900

    コメント文を一部追加した

--- server/module/calc.py
+++ server/module/calc.py
@@ -14,21 +14,26 @@ from gensim.models import fasttext
 
 class NLP_COS_SIMILARITY():
     def __init__(self, nlp_type="BERT"):
-        self.nlp_type = nlp_type
+        self.nlp_type = nlp_type    # 自然言語処理の種類
     
+    # 接続したクライアントとのやり取り
     def connetClient(self, cl, cla, calc_func):
         while True:
             data = cl.recv(1024).decode("utf-8")
             
+            # 送られてくるデータは空白が含まれる文字列と仮定
+            # 例 data = "hoge huga"
             if " " in data:
                 plan, idea = tuple(data.split(" "))
                 cl.send(calc_func(plan, idea).encode("utf-8"))
             elif data == "end":
+                # 特に理由はないがサーバーを閉じる文字列を含める
                 self.server.close()
             else:
+                # 空白の含まれない上記の文字列以外の文字列が来た場合、単純に返信する
                 cl.send("計算失敗".encode("utf-8"))
-        
 
+    # サーバーの立ち上げ    
     def startServer(self, calc_func):
         self.address = (socket.gethostbyname(socket.gethostname()), 59630)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,16 +49,19 @@ class NLP_COS_SIMILARITY():
 
         self.server.close()
 
+    # fastText用の計算部分
     def calcScoreForFastText(self, plan: str, word: str):
         
         return self.model.wv.similarity(plan, word).astype(numpy.unicode)
 
+    # fastTextの立ち上げ
     def fastTextCalc(self):
         self.model_path = "./models/wiki.ja/wiki.ja.bin"
         self.model = fasttext.load_facebook_model(self.model_path)
 
         self.startServer(self.calcScoreForFastText)
 
+    # 処理全体の開始
     def startSimilarity(self):
         if self.nlp_type == "fastText":
             self.fastTextCalc()
